840d9a413e19c166971ffe282417df4a7adb0eea
xa-xa-xa <askrasn@gmail.com>
2019-10-26 13:08:33 -0700

    convert Navbar and UseeItem to func component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt='avatar logo'
+        srcSet=''
+        className='round-img'
+        style={{ width: 60 }}
+      />
+      <h3>{login}</h3>
+      <a href={html_url} className='btn btn-bark btn-sm my-1' aria-label='more'>
+        more
+      </a>
+    </div>
+  );
+};
 
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          srcSet=''
-          className='round-img'
-          style={{ width: 60 }}
-        />
-        <h3>{login}</h3>
-        <a
-          href={html_url}
-          className='btn btn-bark btn-sm my-1'
-          aria-label='more'>
-          more
-        </a>
-      </div>
-    );
-  }
-}
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
