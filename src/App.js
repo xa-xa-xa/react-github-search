bd57167eb9a459873e87d9f14bb787f6811424d9
xa-xa-xa <askrasn@gmail.com>
2019-10-26 08:52:42 -0700

    added nabar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Navbar title='GitHub Search' icon='fab fa-github' />
         <h1>Hello</h1>
       </div>
     );
