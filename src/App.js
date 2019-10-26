5c39bfaffcfca45d6cfdd9ab04e85dbd17ac3d4b
xa-xa-xa <askrasn@gmail.com>
2019-10-26 15:46:06 -0700

    + clearUsers btn

--- src/App.js
+++ src/App.js
@@ -13,23 +13,40 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  /*
+   * Search GitHub Users
+   */
+  searchUsers = async text => {
     this.setState({ loading: true });
+    console.log('from App.js: ' + text);
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
+    this.setState({ users: res.data.items, loading: false });
+  };
+  clearUsers = () => this.setState({ users: [], loading: false });
 
-    this.setState({ users: res.data, loading: false });
-
-    console.log(res.data);
-  }
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar title='GitHub Search' icon='fab fa-github' />
         <div className='container'>
-          <Search />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
     text: ''
   };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
+  };
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form className='form' onSubmit={this.onSubmit}>
@@ -24,6 +34,11 @@ export class Search extends Component {
           />
           <input type='submit' className='btn btn-danger btn-block' />
         </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
