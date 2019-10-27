fa50d078d406df41d17df0b412bd003758d4ceff
xa-xa-xa <askrasn@gmail.com>
2019-10-27 10:45:18 -0700

    added types.js

---
+++ src/context/types.js
@@ -0,0 +1,7 @@
+export const SEARCH_USERS = 'SEARCH_USERS';
+export const GET_USERS = 'GET_USERS';
+export const CLEAR_USERS = 'CLEAR_USERS';
+export const GET_REPOS = 'GET_REPOS';
+export const SHOW_LOADING = 'SHOW_LOADING';
+export const SHOW_ALERT = 'SHOW_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
