b611097b2b89fe09535014a0cb0b6ff34f7fc004
xa-xa-xa <askrasn@gmail.com>
2019-10-27 15:51:17 -0700

    moved search and users > Home.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import User from './components/users/User';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alerts/AlertState';
@@ -23,15 +22,7 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}></Route>
+                <Route exact path='/' component={Home}></Route>
                 <Route exact path='/about' component={About} />
                 <Route path='/user/:login' component={User} />
               </Switch>
