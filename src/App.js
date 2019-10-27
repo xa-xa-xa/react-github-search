14e40e2893f4aa40699e964d256ca470cd65370e
xa-xa-xa <askrasn@gmail.com>
2019-10-27 15:59:04 -0700

    + NotFound

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import User from './components/users/User';
 
 import GithubState from './context/github/GithubState';
@@ -25,6 +26,7 @@ const App = () => {
                 <Route exact path='/' component={Home}></Route>
                 <Route exact path='/about' component={About} />
                 <Route path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h1>Not Found</h1>
+    <p className='lead'>Page that was requested not exist...</p>
+  </div>
+);
+
+export default NotFound;
